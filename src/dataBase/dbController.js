ed14451c54833fdf86654dc298ec3ef3c349ccec
IgnacioC <cardozojoseignacio@gmail.com>
2020-10-12 10:53:41 -0300

    feat Squelize modules
    Instalando modulos sequelize y generando la estructura basica para el funcionamiento del mismo

---
+++ src/dataBase/dbController.js
@@ -0,0 +1,5 @@
+const { Sequelize } = require('sequelize');
+
+// Option 1: Passing a connection URI
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+
