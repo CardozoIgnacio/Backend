362d1a024ec98eaa03ad081b4932959d5de60ff2
Gino <ginorossiar@gmail.com>
2020-10-16 22:42:37 -0300

    Crea modelo Usuario
    Creacion del modelo Usuario

--- src/model/usuario.js
+++ src/model/usuario.js
@@ -1,53 +1,10 @@
-var Sequelize = require("sequelize");
-var db = require("../dataBase/dbController");
+var Usuario= require("../model/usuario")
 
-// 1: The model schema.
-var modelDefinition = {
-	id:{
-		type:Sequelize.INTEGER,
-		autoIncrement:true,
-		primaryKey:true,
-		allowNull:false
-	},
-	usuario: {
-		type: Sequelize.STRING,
-		unique: true,
-		allowNull: false,
-	},
-	password: {
-		type: Sequelize.STRING,
-		allowNull: false,
-   },
-   rol: {
-       type : Sequelize.BIGINT,
-       allowNull: false
-   }
-
-};
-
-// 2: The model options.
-var modelOptions = {
-	instanceMethods: {
-		comparePasswords: comparePasswords,
-	},
-	hooks: {
-		beforeValidate: hashPassword,
-	},
-	timestamps :false,
-	modelName:'usuario',
-	freezeTableName:true
-	
-};
-
-// 3: Define the User model.
-var UsuarioModel = db.define("usuario", modelDefinition, modelOptions);
-
-function comparePasswords(password, callback) {
-	// TODO: Comparar password a nivel modelo requeire instalacion de bycrpt
+async function findUsuario (req,res){
+    console.log(await Usuario.findAll({}))
+    res.send({"hola":"hola"})
 }
+var user = {} 
+user.findUsuario = findUsuario
 
-function hashPassword(user) {
-	// TODO: Encriptacion de password a nivel modelo requiere bycrypt
-}
-
-module.exports = UsuarioModel;
+module.exports= user
