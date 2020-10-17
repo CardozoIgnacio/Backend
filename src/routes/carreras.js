4098efb4fcaf7dcec36bdfa19eaa93df7429340e
Gino <ginorossiar@gmail.com>
2020-10-17 16:06:59 -0300

    Agrega enrutamientos CRUD a Carreras
    Se crearon los enrutamientos correspondientes al controlador que interactua con el modelo Carreras

--- src/routes/carreras.js
+++ src/routes/carreras.js
@@ -2,7 +2,14 @@ var express = require('express');
 var controladorCarrera = require('../controller/controladorCarrera')
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', controladorCarrera.findCarrera);
+/* GET listado carreras. */
+router.get('/', controladorCarrera.listarCarreras_get);
+router.get('/crearCarrera', controladorCarrera.crearCarrera_get);
+router.get('/:id/actualizar', controladorCarrera.actualizarCarrera_get);
+router.get('/mostrar/:id', controladorCarrera.encontrarCarrera_get);
 
+/* POST listado carreras */
+router.post('/', controladorCarrera.crearCarrera_post);
+router.post('/:id/actualizar', controladorCarrera.actualizarCarrera_post);
+router.post('/:id/destruir', controladorCarrera.destruirCarrera_post);
 module.exports = router;
