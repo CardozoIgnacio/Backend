5235b7927e046e28e213239a511a64038113bbad
IgnacioC <cardozojoseignacio@gmail.com>
2020-10-17 12:08:12 -0300

    Refactor exportacion de funciones
    Se modifico la forma en la cual se exportan las funciones para simplificar tanto la lectura como la codificacion de las mismas

--- src/controller/controllerUsuarios.js
+++ src/controller/controllerUsuarios.js
@@ -1,12 +1,12 @@
 var Usuario = require("../model/usuario");
-var user = {};
 
-async function findUsuario(req, res) {
+
+exports.encontrarUsuario =async function (req, res) {
 	console.log(await Usuario.findAll({}));
 	res.send({ hola: "hola" });
 }
 
-async function crearUsuario(req, res) {
+exports.crearUsuario = async function (req, res) {
 	//TODO: Actualizr atributos de Usuarios
 	//TODO: Recuperarr datos del req
 	try {
@@ -23,9 +23,14 @@ async function crearUsuario(req, res) {
 		console.log("El error es", error);
 	}
 }
+exports.actualizarUsuario= async function (req, res) {
+	//TODO: Actualizar atributos de usuariosa
+	//TODO: Recuperar datos del body
+	
+	
+	try {
+		await Usuario.update({usuario:"GenaPauli"},{where:{id:1}});
+	} catch (error) {}
+}
 
 
-
-user.findUsuario = findUsuario;
-user.crearUsuario = crearUsuario;
-module.exports = user;
