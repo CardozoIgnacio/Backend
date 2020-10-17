8ae31803b8192655aba5f0c154d662aa20b0b967
IgnacioC <cardozojoseignacio@gmail.com>
2020-10-17 14:27:51 -0300

    Agrega endpoint CRUD sobre Usuarios
    Se crearon las rutas y los controladores asociados a las misma para pocrer crear usuarios listar ususuarios y destruir usuairos.

--- src/controller/controllerUsuarios.js
+++ src/controller/controllerUsuarios.js
@@ -1,15 +1,35 @@
 var Usuario = require("../model/usuario");
 
-
-exports.encontrarUsuario =async function (req, res) {
-	console.log(await Usuario.findAll({}));
-	res.send({ hola: "hola" });
-}
-
-exports.crearUsuario = async function (req, res) {
+exports.listarUsuarios_get = function (req, res) {
+	//TODO: Renderizado de listar Usuarios
+	res.send({ msj: "Renderizar Pagina de filtrado de usuarios" });
+};
+exports.crearUsuario_get = function (req, res) {
+	//TODO: Rederizado de crear Usuario
+	//TODO: Buscar informacion de AlumnoPendiente
+	res.send({ msj: "Renderizar pagina para crear un usuario" });
+};
+exports.actualizarUsuario_get = function (req, res) {
+	//TODO: Renderizado de Actualizacion de usuarios
+	res.send({ msj: "Renderizar pagina para actualizar usuario" });
+};
+exports.encontrarUsuarios_get = async function (req, res) {
+	//TODO: Verificar permisos a la hora de buscar usuarios
+	try {
+		var id = req.params.id;
+		await Usuario.findAll({ where: { id: id } });
+		//TODO: Renderizar vbista del usuario encontrado
+		res.send({ msj: "Usuario encontrado" });
+	} catch (error) {
+		//TODO :Mensaje de error ControllerUsuario
+		console.log("El error es", error);
+	}
+};
+exports.crearUsuario_post = async function (req, res) {
 	//TODO: Actualizr atributos de Usuarios
 	//TODO: Recuperarr datos del req
 	try {
+		//TODO: Verificar permisos a la hora de crear un Usuario
 		await Usuario.create({
 			usuario: "Genaro",
 			nombre: "Genaro",
@@ -18,19 +38,42 @@ exports.crearUsuario = async function (req, res) {
 			rol: 1,
 			nombreAbreviado: "GP",
 		});
+		res.send({ msj: "Usuario creado exitosamente" });
 	} catch (error) {
 		//TODO :Mensaje de error ControllerUsuario
 		console.log("El error es", error);
 	}
-}
-exports.actualizarUsuario= async function (req, res) {
-	//TODO: Actualizar atributos de usuariosa
-	//TODO: Recuperar datos del body
-	
-	
+};
+exports.actualizarUsuario_post = async function (req, res) {
+	//TODO: Actualizar atributos de usuarios
 	try {
-		await Usuario.update({usuario:"GenaPauli"},{where:{id:1}});
-	} catch (error) {}
-}
-
-
+		var id = req.params.id;
+		//TODO: Verificar permisos a la hora de actualizar un usuario
+		await Usuario.update(
+			{
+				usuario: "Gerardo",
+				nombre: "Gerardo",
+				apellido: "Sardinez",
+				password: "1234",
+				rol: 1,
+				nombreAbreviado: "GS",
+			},
+			{ where: { id: id } }
+		);
+		res.send({ msj: "Usuario actualizado exitosamente" });
+	} catch (error) {
+		//TODO :Mensaje de error ControllerUsuario
+		console.log("El error es", error);
+	}
+};
+exports.destruirUsuario_post = async function (req, res) {
+	var id = req.params.id;
+	try {
+		//TODO: Verificar permisos a la hora de destruir un usuario
+		Usuario.destroy({ where: { id: id } });
+		res.send({ msj: "Usuario destruido exitosamente " });
+	} catch (error) {
+		//TODO :Mensaje de error ControllerUsuario
+		console.log("El error es", error);
+	}
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,14 @@ var controllerUsuarios = require('../controller/controllerUsuarios')
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/',controllerUsuarios.crearUsuario);
+router.get('/',controllerUsuarios.listarUsuarios_get)
+router.get('/crearUsuario',controllerUsuarios.crearUsuario_get)
+router.get('/:id/actualizar',controllerUsuarios.actualizarUsuario_get)
+router.get('/mostrar/:id',controllerUsuarios.encontrarUsuarios_get)
+
+/* POST user listing */
+router.post('/crearUsuario',controllerUsuarios.crearUsuario_post);
+router.post('/:id/actualizar', controllerUsuarios.actualizarUsuario_post)
+router.post('/:id/destruir',controllerUsuarios.destruirUsuario_post)
 
 module.exports = router;
