ea12e220d3e36c1d3eb9ccf68361ec595054c063
Gino <ginorossiar@gmail.com>
2020-10-16 22:40:46 -0300

    Caracteristica modelo Usuario
    Creacion del modelo usuario que utiliza sequelize para mapear la tabla usuarios

--- src/dataBase/dbController.js
+++ src/dataBase/dbController.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 // Option 1: Passing a connection URI
- const sequelize = new Sequelize(process.env.DATABASE_URL) //Example for postgres
-// sequelize.sync({force:true})
-//const sequelize = new Sequelize(process.env.DB,process.env.DB_USER ,process.env.DB_PASS, {    host: 'localhost',    dialect: 'postgres',  });
+const sequelize = new Sequelize(process.env.DATABASE_URL) //Example for postgres
 
+//sequelize.sync({ force: true });
+//const sequelize = new Sequelize(process.env.DB,process.env.DB_USER ,process.env.DB_PASS, {    host: 'localhost',    dialect: 'postgres',  });
 
 module.exports = sequelize
