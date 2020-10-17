9ea0a7ff041de55fc89290be7ee92c36c981d33a
IgnacioC <cardozojoseignacio@gmail.com>
2020-10-17 11:42:52 -0300

    Agrega AsyncFunc CrearUsuario
    Se crea la estructura basica para poder agregar un nuevo usuario en la base de datos

--- src/controller/controllerUsuarios.js
+++ src/controller/controllerUsuarios.js
@@ -1,12 +1,31 @@
-var Usuario= require("../model/usuario")
+var Usuario = require("../model/usuario");
+var user = {};
 
-async function findUsuario (req,res){
-    console.log(await Usuario.findAll({}))
-    res.send({"hola":"hola"})
+async function findUsuario(req, res) {
+	console.log(await Usuario.findAll({}));
+	res.send({ hola: "hola" });
 }
-var user = {} 
-user.findUsuario = findUsuario
 
-module.exports= user
+async function crearUsuario(req, res) {
+	//TODO: Actualizr atributos de Usuarios
+	//TODO: Recuperarr datos del req
+	try {
+		await Usuario.create({
+			usuario: "Genaro",
+			nombre: "Genaro",
+			apellido: "Paulucchi",
+			password: "1234",
+			rol: 1,
+			nombreAbreviado: "GP",
+		});
+	} catch (error) {
+		//TODO :Mensaje de error ControllerUsuario
+		console.log("El error es", error);
+	}
+}
 
 
+
+user.findUsuario = findUsuario;
+user.crearUsuario = crearUsuario;
+module.exports = user;
