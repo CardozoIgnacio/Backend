c9bb4b5a7d5c2857d3f76bc7b232a8b0f7f42d2c
IgnacioC <cardozojoseignacio@gmail.com>
2020-10-19 10:28:04 -0300

    Agrega configuracion incial de Sequelize-Cli

--- src/dataBase/dbController.js
+++ src/dataBase/dbController.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
-
+const config = require('../../config/config')
 // Option 1: Passing a connection URI
-const sequelize = new Sequelize(process.env.DATABASE_URL) //Example for postgres
+const sequelize = new Sequelize(config.database,config.username,config.password,{host:config.host,dialect:'postgres'}) //Example for postgres
 
 sequelize.sync({ force: true });
 //const sequelize = new Sequelize(process.env.DB,process.env.DB_USER ,process.env.DB_PASS, {    host: 'localhost',    dialect: 'postgres',  });
