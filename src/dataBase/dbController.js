64a9a8cff6bf1150de597d57fae80f059fa03845
Gino <ginorossiar@gmail.com>
2020-10-23 14:23:08 -0300

    Deshabilita sincronizacion forzada

--- src/dataBase/dbController.js
+++ src/dataBase/dbController.js
@@ -3,7 +3,7 @@ const config = require('../../config/config')
 // Option 1: Passing a connection URI
 const sequelize = new Sequelize(config.database,config.username,config.password,{host:config.host,dialect:'postgres'}) //Example for postgres
 
-sequelize.sync({ force: true });
+//sequelize.sync({ force: true });
 //const sequelize = new Sequelize(process.env.DB,process.env.DB_USER ,process.env.DB_PASS, {    host: 'localhost',    dialect: 'postgres',  });
 
 module.exports = sequelize
