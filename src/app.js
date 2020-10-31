1f9deea75468811f3b8f2ce852cc8781899f06a3
Gino <ginorossiar@gmail.com>
2020-10-31 20:37:58 -0300

    Se agrega la dependencia de express-session y se realizan configuraciones a la misma

--- src/app.js
+++ src/app.js
@@ -3,17 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+require('../config/passport');
 
 // 2-App Modules
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/usuarios');
- var carrerasRouter = require('./routes/carreras');
- var noticiasRouter = require('./routes/noticias')
+var carrerasRouter = require('./routes/carreras');
+var noticiasRouter = require('./routes/noticias')
 
 // 3-Inicitialization
 var app = express();
 
-// 4-MidelWares
+// 4-MiddleWares
+
+app.use(session({
+   secret: 'clave-secreta',
+   resave: true,
+   saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.json());//Interpreta los datos Json que envie el cliente
 app.use(express.urlencoded({ extended: false }));
