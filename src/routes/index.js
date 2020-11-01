bbddb91f334e3651fdd2d78dc40ee992553dd5c1
Gino <ginorossiar@gmail.com>
2020-10-31 22:20:44 -0300

    Agrega metodo post para la autenticacion de usuarios via passport

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+})
+
+/* POST login */
+
+ router.post('/login', passport.authenticate('local', {
+    successRedirect: "/",
+    failureRedirect: "/login?result=failure"
+}));
 
 module.exports = router;
