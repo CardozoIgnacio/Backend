3586f42fed39edaa67a9358d404f6e4778cdb8a3
IgnacioC <cardozojoseignacio@gmail.com>
2020-11-04 18:25:47 -0300

    Agrega seed crear-tramite
    Se reconfigura la seed crear-tramite para modelar la nueva configuracion, de que los tramites tendran que estar en una tabla diferente a la de tramite

--- src/dataBase/seeders/20201028153421-crear-tramite.js
+++ src/dataBase/seeders/20201028153421-crear-tramite.js
@@ -2,62 +2,78 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    	var tramitesNombres = [
+			{
+				nombreTipoTramite: "ConsultaVarias",
+      },
+      {
+        nombreTipoTramite:"HistorialAcademico"
+      },
+      {
+        nombreTipoTramite:"ConstanciaAlumnoRegular"
+      },
+     
+      {
+        nombreTipoTramite:"InscripcionMateria"
+      },
+		];
+		await queryInterface.bulkInsert("tipoTramite", tramitesNombres, {})
     var tramites=[{
-      "tipoTramite": "ConsultaVarias",
+      "tipoTramite": 1,
       "fechaInicio": "06/02/2020",
       "estado": true,
       "fechaFinalizacion": "21/07/2020",
       "token": "0318104466"
     }, {
-      "tipoTramite": "HistorialAcademico",
+      "tipoTramite": 2,
       "fechaInicio": "29/10/2019",
       "estado": true,
       "fechaFinalizacion": "06/08/2020",
       "token": "6312465969"
     }, {
-      "tipoTramite": "ConsultaVarias",
+      "tipoTramite":1, 
       "fechaInicio": "08/07/2020",
       "estado": true,
       "fechaFinalizacion": "13/05/2020",
       "token": "9866961761"
     }, {
-      "tipoTramite": "ConstanciaAlumnoRegular",
+      "tipoTramite": 3,
       "fechaInicio": "21/06/2020",
       "estado": false,
       "fechaFinalizacion": "10/12/2019",
       "token": "5773286068"
     }, {
-      "tipoTramite": "InscripcionMateria",
+      "tipoTramite": 4,
       "fechaInicio": "21/08/2020",
       "estado": false,
       "fechaFinalizacion": "17/09/2020",
       "token": "6597434161"
     }, {
-      "tipoTramite": "ConstanciaAlumnoRegular",
+      "tipoTramite": 3,
       "fechaInicio": "09/02/2020",
       "estado": false,
       "fechaFinalizacion": "18/10/2020",
       "token": "7213505750"
     }, {
-      "tipoTramite": "InscripcionMateria",
+      "tipoTramite": 4,
       "fechaInicio": "16/06/2020",
       "estado": true,
       "fechaFinalizacion": "19/07/2020",
       "token": "1957941367"
     }, {
-      "tipoTramite": "ConsultaVarias",
+      "tipoTramite": 1,
       "fechaInicio": "26/08/2020",
       "estado": false,
       "fechaFinalizacion": "27/03/2020",
       "token": "9932050776"
     }, {
-      "tipoTramite": "ConstanciaAlumnoRegular",
+      "tipoTramite": 3,
       "fechaInicio": "05/05/2020",
       "estado": true,
       "fechaFinalizacion": "29/10/2019",
       "token": "7407177996"
     }, {
-      "tipoTramite": "ConstanciaAlumnoRegular",
+      "tipoTramite": 3,
       "fechaInicio": "12/07/2020",
       "estado": true,
       "fechaFinalizacion": "02/07/2020",
