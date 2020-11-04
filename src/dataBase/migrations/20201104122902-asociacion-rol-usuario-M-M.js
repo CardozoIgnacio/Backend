186c84e75b43cb75e3199669869022ba17db69fb
IgnacioC <cardozojoseignacio@gmail.com>
2020-11-04 09:51:26 -0300

    Agrega asociacion entre Usuarios y Roles
    SE define la asociacion M-M entre usuarios y roles, tambien se agregan las migraciones correspondientes para la reacion de dicha asociacion en la base de datos, de tal menerra que se respete la existencia de Usuarios y Roles al momento de insertar en la tabla rolUsuario

---
+++ src/dataBase/migrations/20201104122902-asociacion-rol-usuario-M-M.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("rolUsuario", {
+			idRol: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				allowNull: false,
+				references: {
+					model: "Rol",
+					key: "idRol",
+				},
+			},
+			idUsuario: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				references: {
+					model: "Usuario",
+					key: "id",
+				},
+			},
+		});
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("users");
+	},
+};
---
+++ src/model/rolusuario.js
@@ -0,0 +1,40 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class rolUsuario extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	rolUsuario.init(
+		{
+			idRol: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				allowNull: false,
+				references: {
+					model: sequelize.Rol,
+					key: "idRol",
+				},
+			},
+			idUsuario: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				references: {
+					model: sequelize.Usuario,
+					key: "id",
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "rolUsuario",
+		}
+	);
+	return rolUsuario;
+};
