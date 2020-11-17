aff53156b342a11bcb837b5ce5e5373283eae3a7
Gino <ginorossiar@gmail.com>
2020-11-17 17:25:19 -0300

    Agrega renderizacion de noticias dentro de index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var { Noticia } = require("../model/index");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  try {
+    await Noticia.findAll({})
+      .then(noticias => {
+        res.render('index', { noticias: noticias });
+      })
+      .catch(error => {
+        console.log("Error al renderizar las noticias. Se renderizará index sin mostrarlas.");
+        res.render('index');
+      });
+  } catch {
+    console.log("Error al renderizar las noticias. Se renderizará index sin mostrarlas.");
+    res.render('index');
+  }
 });
 
 router.get('/login', function(req, res, next) {
