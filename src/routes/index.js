2c510c701a3d8d255b39cffc2cba97acb1d8483c
Gino <ginorossiar@gmail.com>
2020-11-18 14:36:25 -0300

    Agrega enrutamiento a la vista de Perfil

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,24 +5,29 @@ var { Noticia } = require("../model/index");
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
+
   try {
     await Noticia.findAll({})
       .then(noticias => {
-        res.render('index', { noticias: noticias });
+        res.render('index', { noticias: noticias, usuario: req.user });
       })
       .catch(error => {
         console.log("Error al renderizar las noticias. Se renderizará index sin mostrarlas.");
-        res.render('index');
+        res.render('index', { usuario: req.user });
       });
   } catch {
     console.log("Error al renderizar las noticias. Se renderizará index sin mostrarlas.");
-    res.render('index');
+    res.render('index', { usuario: req.user });
   }
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  res.render('login', { usuario: req.user });
 })
+
+router.get('/perfil', function(req, res, next) {
+  res.render('perfil', { usuario: req.user });
+});
 
 /* POST login */
 
