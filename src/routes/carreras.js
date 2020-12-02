83230ce5a1587f5c0c1c52a6b571411dcfd4d320
Gino <ginorossiar@gmail.com>
2020-12-02 19:27:28 -0300

    Agrega funcionalidades al enrutamiento a carreras

--- src/routes/carreras.js
+++ src/routes/carreras.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var multer = require("multer");
+var upload = multer({ dest: 'public/carreras/todas/ '});
 var controladorCarrera = require('../controller/controladorCarrera')
 var router = express.Router();
 
@@ -9,7 +11,7 @@ router.get('/:id/actualizar', controladorCarrera.actualizarCarrera_get);
 router.get('/mostrar/:id', controladorCarrera.encontrarCarrera_get);
 
 /* POST listado carreras */
-router.post('/', controladorCarrera.crearCarrera_post);
+router.post('/', upload.single('miniatura'), controladorCarrera.crearCarrera_post);
 router.post('/:id/actualizar', controladorCarrera.actualizarCarrera_post);
 router.post('/:id/destruir', controladorCarrera.destruirCarrera_post);
 module.exports = router;
