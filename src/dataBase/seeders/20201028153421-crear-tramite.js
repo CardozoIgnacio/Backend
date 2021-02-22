892261dd77d634aa20e278fe1b90a4bd2508f087
IgnacioC <cardozojoseignacio@gmail.com>
2021-02-22 09:30:12 -0300

    Actualiza sembrado de tablas
    Se realizaron modifiaciones en el sembrado de las tablas con el fin de eliminar datos de testing

--- src/dataBase/seeders/20201028153421-crear-tramite.js
+++ src/dataBase/seeders/20201028153421-crear-tramite.js
@@ -18,83 +18,83 @@ module.exports = {
       },
 		];
 		await queryInterface.bulkInsert("TipoTramite", tramitesNombres, {})
-    var tramites=[{
-      "tipoTramite": 1,
-      "fechaInicio": "06/02/2020",
-      "estado": true,
-      "fechaFinalizacion": "21/07/2020",
-      "token": "0318104466",
-      "idUsuario":1
+    // var tramites=[{
+    //   "tipoTramite": 1,
+    //   "fechaInicio": "06/02/2020",
+    //   "estado": true,
+    //   "fechaFinalizacion": "21/07/2020",
+    //   "token": "0318104466",
+    //   "idUsuario":1
 
-    }, {
-      "tipoTramite": 2,
-      "fechaInicio": "29/10/2019",
-      "estado": true,
-      "fechaFinalizacion": "06/08/2020",
-      "token": "6312465969",
-      "idUsuario":1
+    // }, {
+    //   "tipoTramite": 2,
+    //   "fechaInicio": "29/10/2019",
+    //   "estado": true,
+    //   "fechaFinalizacion": "06/08/2020",
+    //   "token": "6312465969",
+    //   "idUsuario":1
 
-    }, {
-      "tipoTramite":1, 
-      "fechaInicio": "08/07/2020",
-      "estado": true,
-      "fechaFinalizacion": "13/05/2020",
-      "token": "9866961761",
-      "idUsuario":1
+    // }, {
+    //   "tipoTramite":1, 
+    //   "fechaInicio": "08/07/2020",
+    //   "estado": true,
+    //   "fechaFinalizacion": "13/05/2020",
+    //   "token": "9866961761",
+    //   "idUsuario":1
 
-    }, {
-      "tipoTramite": 3,
-      "fechaInicio": "21/06/2020",
-      "estado": false,
-      "fechaFinalizacion": "10/12/2019",
-      "token": "5773286068",
-      "idUsuario":1
+    // }, {
+    //   "tipoTramite": 3,
+    //   "fechaInicio": "21/06/2020",
+    //   "estado": false,
+    //   "fechaFinalizacion": "10/12/2019",
+    //   "token": "5773286068",
+    //   "idUsuario":1
 
-    }, {
-      "tipoTramite": 4,
-      "fechaInicio": "21/08/2020",
-      "estado": false,
-      "fechaFinalizacion": "17/09/2020",
-      "token": "6597434161",
-      "idUsuario":3
-    }, {
-      "tipoTramite": 3,
-      "fechaInicio": "09/02/2020",
-      "estado": false,
-      "fechaFinalizacion": "18/10/2020",
-      "token": "7213505750",
-      "idUsuario":2
-    }, {
-      "tipoTramite": 4,
-      "fechaInicio": "16/06/2020",
-      "estado": true,
-      "fechaFinalizacion": "19/07/2020",
-      "token": "1957941367",
-      "idUsuario":2
-    }, {
-      "tipoTramite": 1,
-      "fechaInicio": "26/08/2020",
-      "estado": false,
-      "fechaFinalizacion": "27/03/2020",
-      "token": "9932050776",
-      "idUsuario":2
-    }, {
-      "tipoTramite": 3,
-      "fechaInicio": "05/05/2020",
-      "estado": true,
-      "fechaFinalizacion": "29/10/2019",
-      "token": "7407177996",
-      "idUsuario":2
-    }, {
-      "tipoTramite": 3,
-      "fechaInicio": "12/07/2020",
-      "estado": true,
-      "fechaFinalizacion": "02/07/2020",
-      "token": "5377181549",
-      "idUsuario":3
-    }]
-      await queryInterface.bulkInsert('Tramite',tramites, 
-      {});
+    // }, {
+    //   "tipoTramite": 4,
+    //   "fechaInicio": "21/08/2020",
+    //   "estado": false,
+    //   "fechaFinalizacion": "17/09/2020",
+    //   "token": "6597434161",
+    //   "idUsuario":3
+    // }, {
+    //   "tipoTramite": 3,
+    //   "fechaInicio": "09/02/2020",
+    //   "estado": false,
+    //   "fechaFinalizacion": "18/10/2020",
+    //   "token": "7213505750",
+    //   "idUsuario":2
+    // }, {
+    //   "tipoTramite": 4,
+    //   "fechaInicio": "16/06/2020",
+    //   "estado": true,
+    //   "fechaFinalizacion": "19/07/2020",
+    //   "token": "1957941367",
+    //   "idUsuario":2
+    // }, {
+    //   "tipoTramite": 1,
+    //   "fechaInicio": "26/08/2020",
+    //   "estado": false,
+    //   "fechaFinalizacion": "27/03/2020",
+    //   "token": "9932050776",
+    //   "idUsuario":2
+    // }, {
+    //   "tipoTramite": 3,
+    //   "fechaInicio": "05/05/2020",
+    //   "estado": true,
+    //   "fechaFinalizacion": "29/10/2019",
+    //   "token": "7407177996",
+    //   "idUsuario":2
+    // }, {
+    //   "tipoTramite": 3,
+    //   "fechaInicio": "12/07/2020",
+    //   "estado": true,
+    //   "fechaFinalizacion": "02/07/2020",
+    //   "token": "5377181549",
+    //   "idUsuario":3
+    // }]
+    //   await queryInterface.bulkInsert('Tramite',tramites, 
+    //   {});
     
   },
 
